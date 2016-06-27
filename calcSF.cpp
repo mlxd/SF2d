68e2241b5b2abafb28006a0d6fa98e3d1cc381e5
mlxd <loriordan@gmail.com>
2016-06-27 15:28:27 +0900

    Restructured and added oenmp. Maybe borked

--- calcSF.cpp
+++ calcSF.cpp
@@ -33,6 +33,7 @@
 #include <iomanip>
 #include <string.h>
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 
@@ -50,18 +51,18 @@ class CVortex{
 	~CVortex(){}
 
 	void set_pos (double a,double b,double c){
-			x = a; y = b; z = c;
+		x = a; y = b; z = c;
 	}
 
-    double get_x(){
+    	double get_x(){
 			return x;
 	}
 
-    double get_y(){
+    	double get_y(){
 			return y;
 	}
 
-    double get_z(){
+    	double get_z(){
 			return z;
 	}
 };
@@ -77,8 +78,9 @@ void initialiseVortices(list<CVortex>& vorticesList, bool& file){
 	cout << renderStr << endl;
 	strcpy(renderChar,renderStr.c_str());
 
-    ifstream myfile (renderChar);
-    cout << "initialise Vortices" << endl;
+	ifstream myfile (renderChar);
+	cout << "initialise Vortices" << endl;
+
 
 	if (myfile.is_open()) {
 		cout << "Initial Vortex Positions From File" << endl;
@@ -87,17 +89,14 @@ void initialiseVortices(list<CVortex>& vorticesList, bool& file){
 		double yval;
 		int a,b,c;
 		char delim;
-        while ( myfile.good() )
-        {
-            myfile >> xval;
+		while ( myfile.good() ){
+       		 	myfile >> xval;
 			xval *= 6.821230e-07;
 			myfile >> delim;
 
-            myfile >> yval;
+            		myfile >> yval;
 			yval *= 6.821230e-07;
 			myfile >> delim;
-
-			//cout << "XVAL=" << xval << ", YVAL=" << yval << std::endl;
 
 			myfile >> a;
 			myfile >> delim;
@@ -106,15 +105,15 @@ void initialiseVortices(list<CVortex>& vorticesList, bool& file){
 			myfile >> delim;
 
 			myfile >> c;
-            CVortex newVortex;
-            if (xval > 200*6.821230e-07 && xval < 800*6.821230e-07 && yval > 200*6.821230e-07 && yval < 800*6.821230e-07 ) {
+			CVortex newVortex;
+			if (xval > 200*6.821230e-07 && xval < 800*6.821230e-07 && yval > 200*6.821230e-07 && yval < 800*6.821230e-07 ) {
 				newVortex.set_pos(xval/a0,yval/a0,0);
 				vorticesList.push_back(newVortex);
 			}
-        }
-        myfile.close();
+       		}
+        	myfile.close();
 	}
-    cout << "initialised" << endl;
+	cout << "initialised" << endl;
 }
 
 double S(double qx, double qy, list<CVortex> posdata) {
@@ -125,39 +124,76 @@ double S(double qx, double qy, list<CVortex> posdata) {
 
 		for (list<CVortex>::iterator q=posdata.begin(); q!=posdata.end(); ++q) {
 			sumq +=
-                    cos( qx * (q->get_x() - p->get_x())
-                    +
-                    qy * (q->get_y() - p->get_y())
-                );
+			cos( qx * (q->get_x() - p->get_x())
+			+
+			qy * (q->get_y() - p->get_y())
+		);
 		}
-	    sumpq += sumq;
+		sumpq += sumq;
 	}
 	return sumpq;
 }
 
 
 int main() {
-	double numRecip=7.0;
+	int ngX=128;
+	double numRecip=2*pi;
 	list<CVortex> posdata;
 
 	bool file;
 	initialiseVortices(posdata, file);
-	ofstream plotdata;
-	plotdata.open("SFplotdata.txt");
-    plotdata.precision(6);
-    cout << "posdata size: " << posdata.size() << endl;
-	double sf;
-	for (double qx = -numRecip*pi; qx<=numRecip*pi; qx += 2*pi/80) {
-		for (double qy = -numRecip*pi; qy<=numRecip*pi; qy += 2*pi/80) {
-			sf=S(qx,qy,posdata);
+	double sf,qx,qy;
+	int qi,qj;
+	std::vector<double> vx(ngX);
+	std::vector<double> vy(ngX);
+	std::vector<double> vz(ngX*ngX);
+	#pragma omp parallel for private(qj)
+	for (qi = 0; qi<ngX; qi++) {
+		for (qj = 0; qj<ngX; qj++) {
+
+			qx = ((qi-ngX/2.)/ngX)*numRecip*pi;
+			vx[qi] = qx;
+			qy = ((qj-ngX/2.)/ngX)*numRecip*pi;
+			vy[qj] = qy;
+			vz[qi*ngX+qj] = S(qx,qy,posdata);
+
+			//plotdata << setw(20) << qx << setw(20) << qy << setw(20) << vz[qi*256+qj] << endl;
+			//sf=S(qx,qy,posdata);
+
+
+	//for (qx = -numRecip*pi; qx<=numRecip*pi; qx += 2*pi/20) {
+	//	for (qy = -numRecip*pi; qy<=numRecip*pi; qy += 2*pi/20) {
+			//sf=S(qx,qy,posdata);
 			//cout << setw(10) << qx << setw(10) << qy << setw(20) << sf << endl;
-			plotdata << setw(20) << qx << setw(20) << qy << setw(20) << sf << endl;
+			//plotdata << setw(20) << qx << setw(20) << qy << setw(20) << sf << endl;
 		}
-		plotdata << endl;
+		//plotdata << endl;
+	}
+	
+	ofstream dataX, dataY, dataZ;
+	dataX.open("x.csv");
+	dataY.open("y.csv");
+	dataZ.open("z.csv");
+	dataX.precision(6);
+	dataY.precision(6);
+	dataZ.precision(6);
+	
+	for (int ii=0; ii<ngX; ++ii){
+
+		for (int jj=0; jj<ngX; ++jj){
+		
+			dataX << setw(20) << vx[ii] << endl;
+			dataY << setw(20) << vy[jj] << endl;
+			dataZ << vx[ii] << setw(20) << vy[jj] << setw(20) << vz[ii*ngX + jj] << endl;
+
+		}
+		dataZ << endl;
 	}
 
-    plotdata.close();
-    cout << "Structure Factor plot data written to SFplotdata.txt" << endl;
+	dataX.close();
+	dataY.close();
+	dataZ.close();
+	cout << "Structure Factor plot data written to SFplotdata.txt" << endl;
 
 	return 0;
 }
